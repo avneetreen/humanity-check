f79d9151eb58c9fd32d38ad92923b5da57d33f50
avneetreen <avneet14027@iiitd.ac.in>
2024-02-05 21:03:45 +0100

    chore: add data

--- src/data_extraction/scraper.py
+++ src/data_extraction/scraper.py
@@ -1,8 +1,10 @@
 from newsplease import NewsPlease, NewsArticle
 import json
-import os 
-import csv 
-import logging 
+import os
+import csv
+import logging
+import pandas as pd
+
 
 def retrieve_article_from_url(url: str) -> NewsArticle:
     """Retrieve text of article given url
@@ -13,9 +15,13 @@ def retrieve_article_from_url(url: str) -> NewsArticle:
     Returns:
         NewsPlease.from_url: object of type
     """
-    article = NewsPlease.from_url(url, timeout=4)
+    try:
+        article = NewsPlease.from_url(url, timeout=4)
+    except Exception as e:
+        return "None"
     return article
-    
+
+
 def extract_articles_from_csvs(filename: str):
     """Extract article objects given urls in csvs
 
@@ -26,29 +32,36 @@ def extract_articles_from_csvs(filename: str):
         None
     """
     list_of_articles = []
-    with open(os.path.join("../data/links/",filename), "r") as csvFile:
-        reader = csv.DictReader(csvFile)
-        count = 0
-        out_file = open("../data/data.jsonl", 'w', newline='\n')
-        for line in reader: 
-            count+=1
-            link = line['Link']
-            try: 
-                article = retrieve_article_from_url(link)
-                out_file.write(str(article.get_serializable_dict()))
-                out_file.write("\n")
+    # with open(os.path.join("data/links/mediacloud", filename), "r") as csvFile:
+    # reader = csv.DictReader(csvFile)
+    # csvFile.close()
+    data = pd.read_csv(os.path.join("data/links/mediacloud",
+                                    filename))
+    article_count = 1
+    count = 0
+    for index, row in data.iterrows():
+        count += 1
+        link = row['url']
+        try:
+            article = retrieve_article_from_url(link)
+            with open(f"data/US/{article_count}.json", 'a+', newline='\n') as f:
+                json.dump(article.get_serializable_dict(), f)
+                article_count += 1
+                f.close()
+                # out_file.dump(str(article.get_serializable_dict()))
+                # out_file.write(str(article.get_serializable_dict()))
+                # out_file.write("\n")
                 print(article.title)
                 print(count)
-            except Exception as e:
-                print("Unable to get data from: ", link)
-                print("Exception: ", e)
-                continue
+        except Exception as e:
+            print("Unable to get data from: ", link)
+            print("Exception: ", e)
+            continue
     return
 
-    
-            
-    
-if __name__ == "__main__": 
-    for filename in os.listdir("../data/links"):
-        print(filename)
-        extract_articles_from_csvs(filename)
+
+if __name__ == "__main__":
+    # for filename in os.listdir("../data/links"):
+    #    print(filename)
+    filename = "mc-onlinenews-mediacloud-20240201135145-content_USA.csv"
+    extract_articles_from_csvs(filename)
