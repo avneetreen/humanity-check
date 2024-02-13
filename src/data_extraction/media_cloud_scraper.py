3cc0e7ff75d90a8da310c74f55d2429784bc08f2
avneetreen <avneet14027@iiitd.ac.in>
2024-02-13 14:35:45 +0100

    chore: update scraper

--- src/data_extraction/media_cloud_scraper.py
+++ src/data_extraction/media_cloud_scraper.py
@@ -18,7 +18,7 @@ def retrieve_article_from_url(url: str) -> NewsArticle:
     try:
         article = NewsPlease.from_url(url, timeout=4)
     except Exception as e:
-        return "None"
+        print(e)
     return article
 
 
@@ -31,10 +31,6 @@ def extract_articles_from_csvs(filename: str):
     Returns:
         None
     """
-    list_of_articles = []
-    # with open(os.path.join("data/links/mediacloud", filename), "r") as csvFile:
-    # reader = csv.DictReader(csvFile)
-    # csvFile.close()
     data = pd.read_csv(os.path.join("data/links/mediacloud",
                                     filename))
     article_count = 1
@@ -44,18 +40,15 @@ def extract_articles_from_csvs(filename: str):
         link = row['url']
         try:
             article = retrieve_article_from_url(link)
-            with open(f"data/US/{article_count}.json", 'a+', newline='\n') as f:
+            outfile_name = outfile + f"{article_count}.json"
+            with open(outfile_name, 'a+', newline='\n') as f:
                 json.dump(article.get_serializable_dict(), f)
                 article_count += 1
                 f.close()
-                # out_file.dump(str(article.get_serializable_dict()))
-                # out_file.write(str(article.get_serializable_dict()))
-                # out_file.write("\n")
                 print(article.title)
                 print(count)
         except Exception as e:
-            print("Unable to get data from: ", link)
-            print("Exception: ", e)
+            print(e)
             continue
     return
 
@@ -63,5 +56,6 @@ def extract_articles_from_csvs(filename: str):
 if __name__ == "__main__":
     # for filename in os.listdir("../data/links"):
     #    print(filename)
+    outfile = "data/US/"
     filename = "mc-onlinenews-mediacloud-20240201135145-content_USA.csv"
     extract_articles_from_csvs(filename)
