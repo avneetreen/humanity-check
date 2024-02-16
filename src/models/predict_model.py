0bd294d8ea8f1ce9cd8de598110afb9679ed5573
avneetreen <avneet14027@iiitd.ac.in>
2024-02-16 20:00:59 +0100

    chore: inital cleanup

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -2,12 +2,14 @@ import json
 import time
 import openai
 
+
 def read_from_jsonl(file_name):
     with open(file_name, 'r') as f:
         data = []
         for line in f:
             data.append(json.loads(line))
     return data
+
 
 def write_ouput_to_jsonl(article, output_text):
     with open('..data/output.jsonl', 'a') as f:
@@ -16,8 +18,9 @@ def write_ouput_to_jsonl(article, output_text):
         output = {**article_dict, **article_ouput}
         json.dump(output, f)
 
-        
+
 prompt = "Find instances of dehumanising language, biased language or active words being used for one group while passive words being used for the other group. For instance, X are murdered but Y die where X and Y are groups of people. Structure your output in a json format with the violating category as well as the relevant excerpt from the article. Output nothing else except for the explicit answer in json format. Do this for the article below:\n"
+
 
 def main():
     data = read_from_jsonl('..data/data.jsonl')
@@ -30,10 +33,7 @@ def main():
 
         gpt_ouput = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
-            temperature = 0.7,
+            temperature=0.7,
             messages=[{"role": "user", "content": prompt+combined_article}])
         output_text = gpt_ouput.choices[0].message.content
         write_ouput_to_jsonl(article, output_text)
-
-
-    
